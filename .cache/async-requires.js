8e12dd27a37a959f75c2c784c78cc7b6759dae76
Sebastian Selling <sebastian.selling@outlook.com>
2021-08-30 09:00:31 +0200

    init commit

---
+++ .cache/async-requires.js
@@ -0,0 +1,22 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/sebastianselling/Github/houseofselling.com/.cache/dev-404-page.js"),
+  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/sebastianselling/Github/houseofselling.com/src/pages/404.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/sebastianselling/Github/houseofselling.com/src/pages/index.js"),
+  "component---src-pages-page-2-js": require("gatsby-module-loader?name=component---src-pages-page-2-js!/Users/sebastianselling/Github/houseofselling.com/src/pages/page-2.js")
+}
+
+exports.json = {
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/sebastianselling/Github/houseofselling.com/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/sebastianselling/Github/houseofselling.com/.cache/json/dev-404-page.json"),
+  "404.json": require("gatsby-module-loader?name=path---404!/Users/sebastianselling/Github/houseofselling.com/.cache/json/404.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/Users/sebastianselling/Github/houseofselling.com/.cache/json/index.json"),
+  "page-2.json": require("gatsby-module-loader?name=path---page-2!/Users/sebastianselling/Github/houseofselling.com/.cache/json/page-2.json"),
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/sebastianselling/Github/houseofselling.com/.cache/json/404-html.json")
+}
+
+exports.layouts = {
+  "layout---index": require("gatsby-module-loader?name=component---src-layouts-index-js!/Users/sebastianselling/Github/houseofselling.com/.cache/layouts/index.js")
+}
---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'House of Selling',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
